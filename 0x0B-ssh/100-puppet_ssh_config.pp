68413a28fdd1711db757f01becb8bd17b4dd51ba
Blaq Swan <mr.alangobryan@gmail.com>
2022-09-19 02:22:23 +0300

    puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Configure ssh not to use password
 
-file_line {'password auth'
+file_line {'password auth':
 	path => '/etc/ssh/ssh_config',
 	match => '^PasswordAuthentication',
 	line => 'PasswordAuthentication no',
 }
 
-file_line {'set identity file'
+file_line {'set identity file':
 	path => '/etc/ssh/ssh_config',
 	match => '^IdentityFile',
 	line => 'IdentityFile ~/.ssh/school',
